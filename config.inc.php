9a712e6f113079fc463de45918ea974cde7e404e
javidu <javinovich_12@hotmail.com>
2018-09-20 21:43:44 -0500

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -26,7 +26,7 @@ $i = 0;
  */
 $i++;
 /* Authentication type */
-$cfg['Servers'][$i]['auth_type'] = 'cookie';
+$cfg['Servers'][$i]['auth_type'] = 'http';
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = 'mysql-javier.a3c1.starter-us-west-1.openshiftapps.com';
 $cfg['Servers'][$i]['compress'] = false;
